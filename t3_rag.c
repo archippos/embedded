170e24a9d1f6323b9fdd761e553c1229e2702a62
epgirl5 <emp320@msstate.edu>
2021-02-15 19:49:35 -0600

    inludes for the esos_f14 files

--- t3_rag.c
+++ t3_rag.c
@@ -9,6 +9,8 @@ PREPARED FOR: KEITH POWELL
 
 #include    "esos.h"
 #include "revF14.h"
+#include "esos_f14ui.h"
+#include "esos_f14ui.c"
 #ifdef __linux
 #include    "esos_pc.h"
 #include    "esos_pc_stdio.h"
@@ -32,33 +34,6 @@ PREPARED FOR: KEITH POWELL
 // DEFINEs go here
 #ifndef __linux
 
-
-// #define   CONFIG_LED1()   CONFIG_RF4_AS_DIG_OUTPUT()
-// #define   LED1            _LATF4
-// #define   CONFIG_LED2()   CONFIG_RB14_AS_DIG_OUTPUT()
-// #define   LED2            _LATB14
-// #define   CONFIG_LED3()   CONFIG_RB15_AS_DIG_OUTPUT()
-// #define   LED3            _LATB15
-
-// #define   CONFIG_SW1()    {   CONFIG_RB13_AS_DIG_INPUT();  \
-                              // ENABLE_RB13_PULLUP();  \
-                              // DELAY_US(1);  \
-                          // }
-// #define   SW1             _RB13
-
-
-// #define   CONFIG_SW2()    {   CONFIG_RB12_AS_DIG_INPUT();  \
-                              // ENABLE_RB12_PULLUP();  \
-                              // DELAY_US(1);  \
-                          // }
-// #define   SW2             _RB12
-
-
-// #define   CONFIG_SW3()    {   CONFIG_RC15_AS_DIG_INPUT();  \
-                              // ENABLE_RC15_PULLUP();  \
-                              // DELAY_US(1);  \
-                          // }
-// #define   SW3             _RC15
 
 
 

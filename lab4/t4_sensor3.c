6783074f6c0f64b12ba11685ae8d7526282b498d
archippos <deceb.z@gmail.com>
2021-02-20 15:12:26 -0600

    templating

---
+++ lab4/t4_sensor3.c
@@ -0,0 +1,19 @@
+//team "keith help"
+//eli lawrence, erin parker, carol pollard, kane simmons, drew stork
+//t4_sensor3.c - ditto sensor2 but display temperature
+
+//includes
+#include "revF14.h"
+#include "esos.h"
+#include "esos_pic24.h"
+#include "esos_f14ui.h"
+#include "esos_sensor.h"
+#include "esos_pic24_sensor.h"
+
+//TODO: HEARTBEAT (TIMER)
+
+//TODO: DISPLAY OUTPUT (TASK) <- output temperatures here
+
+//TODO: ADC INTERFACE (TASK)
+
+//TODO: USER_INIT (REGISTER TASKS)

9f0fd4b63dae901610b1433f84b936bf448b6a93
archippos <deceb.z@gmail.com>
2021-02-20 13:27:07 -0600

    ADC defines added; added missing endif

--- revF14.h
+++ revF14.h
@@ -60,3 +60,40 @@
 //high/low for B
 #define RPGB_LOW (_RB9 == 0)
 #define RPGB_HIGH (_RB9 == 1)
+
+//configure for analog periphs
+#define ANALOG_CONFIG()                                                                                                \
+    {                                                                                                                  \
+        CONFIG_RB2_AS_ANALOG();                                                                                        \
+        CONFIG_RB3_AS_ANALOG();                                                                                        \
+    }
+//good buddy landon casey says some stuff might need to be changed
+//but ill burn that bridge when i get to it
+#define ADC_CONFIG()                                                                                           \
+    {                                                                                                                  \
+        /* AD1CON1 */                                                                                                  \
+        AD1CON1bits.ADON = 0;                                                                                          \
+        AD1CON1bits.ADSIDL = 0; /* Continue operation in Idle mode */                                                  \
+        AD1CON1bits.ADDMABM = 1; /* DMA Buffers are written in order of conversion */                                  \
+        AD1CON1bits.AD12B = 1; /* Enable 12 bit mode */                                                                \
+        AD1CON1bits.FORM = 0; /* Integer Output Mode */                                                                \
+        AD1CON1bits.SSRC = 0b111; /* Enable Auto-Conversion */                                                         \
+        AD1CON1bits.SSRCG = 0; /* Sample clock source group */                                                         \
+        AD1CON1bits.ASAM = 0; /* Do not auto sample */                                                                 \
+        AD1CON1bits.SAMP = 0; /* Turn off Sample */                                                                    \
+        AD1CON1bits.DONE = 0; /* Clear Done flag */                                                                    \
+        /* AD1CON2 */                                                                                                  \
+        AD1CON2bits.VCFG = 0b001; /* Use external Vref+ and internal Vref- */                                          \
+        AD1CON2bits.CSCNA = 0; /* Do not scan inputs */                                                                \
+        AD1CON2bits.CHPS = 0b00; /* Channel select bits */                                                             \
+        AD1CON2bits.SMPI = 0b00000; /* Generate interrupt on every conversion */                                       \
+        AD1CON2bits.BUFM = 0;                                                                                          \
+        AD1CON2bits.ALTS = 0;                                                                                          \
+        /* AD1CON3 */                                                                                                  \
+        AD1CON3bits.ADRC = 1;                                                                                          \
+        AD1CON3bits.SAMC = 0b11111; /* set Tad to 31 */                                                                \
+        /* AD1CON4 */                                                                                                  \
+        AD1CON4bits.ADDMAEN = 0;                                                                                       \
+        AD1CON1bits.ADON = 1;                                                                                          \
+    }
+#endif

bc40bd717ef77b9ef81ba31b74dacd0f084a1465
archippos <deceb.z@gmail.com>
2021-02-20 14:59:37 -0600

    added intro info for file

---
+++ lab4/t4_sensor1.c
@@ -0,0 +1,6 @@
+//team "keith help"
+//eli lawrence, erin parker, carol pollard, kane simmons, drew stork
+//t4_sensor.c - flashes a heartbeat LED every 250ms
+//              displays potentiometer value in 16b hex when SW1_PRESSED
+//              SW2_PRESSED samples potentiometer voltage every 1s until
+//                          SW2 or SW1 pressed

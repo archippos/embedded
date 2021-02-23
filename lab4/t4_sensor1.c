5cd254ff1845ad57c60f9811f4f7a1a995b10832
archippos <deceb.z@gmail.com>
2021-02-23 17:02:28 -0600

    completed info task

--- lab4/t4_sensor1.c
+++ lab4/t4_sensor1.c
@@ -11,6 +11,7 @@
 #include "esos_pic24_sensor.h"
 
 static uint8_t u8_state;
+static uint16_t pu16_hexOut
 
 // heartbeat on LED 3
 ESOS_USER_TASK(heartbeat)
@@ -38,17 +39,27 @@ ESOS_USER_TASK(info)
       ESOS_ALLOCATE_CHILD_TASK(getADC);
       //TODO: is VREF_3V0 ok? should it be 3V3? or lower? will need to check datasheets
       ESOS_TASK_SPAWN_AND_WAIT(getADC, _WAIT_ON_AVAILABLE_SENSOR, POT_CHANNEL, ESOS_SENSOR_VREF_3V0);
+      ESOS_TASK_SPAWN_AND_WAIT(getADC, _WAIT_SENSOR_QUICK_READ, &pu16_hexOut);
       ESOS_SENSOR_CLOSE();                      //read once, close the sensor channel
       ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();   //now we wait to send our data
-      //[logic to send out output as a hex string goes here]
-			//This logic was used in lab 3
+      //[logic to send out output as a hex string goes here] //This logic was used in lab 3
+			ESOS_TASK_WAIT_ON_SEND_UINT32_AS_HEX_STRING(pu16_hexOut);
+      ESOS_TASK_WAIT_ON_SEND_UINT8('\n');      //slap in a newline to made it purty
       ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();   //all done!
       u8_state = 0;                            //goto state 0 to yield
     } else if(u8_state == 2) {
-    //if u8_state case 2: output every 1s until "state" flag unset
-    ESOS_ALLOCATE_CHILD_TASK(getADC);
-    ESOS_TASK_SPAWN_AND_WAIT(getADC, _WAIT_ON_AVAILABLE_SENSOR, POT_CHANNEL, ESOS_SENSOR_VREF_3V0);
-        //use a do-while(state==2)???
+      //if u8_state case 2: output every 1s until "state" flag unset
+      ESOS_ALLOCATE_CHILD_TASK(getADC);
+      ESOS_TASK_SPAWN_AND_WAIT(getADC, _WAIT_ON_AVAILABLE_SENSOR, POT_CHANNEL, ESOS_SENSOR_VREF_3V0);
+      do {    //use a do-while(state==2)???
+        ESOS_TASK_SPAWN_AND_WAIT(getADC, _WAIT_SENSOR_QUICK_READ, &pu16_hexOut);
+        ESOS_TASK_WAIT_ON_AVAILABLE_OUT_COMM();
+        ESOS_TASK_WAIT_ON_SEND_UINT32_AS_HEX_STRING(pu16_hexOut);
+        ESOS_TASK_WAIT_ON_SEND_UINT8('\n');     //newline :)
+        ESOS_TASK_SIGNAL_AVAILABLE_OUT_COMM();
+        ESOS_TASK_WAIT_TICKS(1000);             //1000ms = 1s
+      } while (u8_state == 2);
+      ESOS_SENSOR_CLOSE();
     }
     ESOS_TASK_YIELD();
   }

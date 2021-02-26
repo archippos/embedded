1a20cc715d625eacb74994f487b6117e45e385a2
archippos <deceb.z@gmail.com>
2021-02-26 15:05:39 -0600

    Create temperature_lcd.c

---
+++ lab5/temperature_lcd.c
@@ -0,0 +1,6 @@
+//team "keith help"
+//eli lawrence, erin parker, carol pollard, kane simmons, drew stork
+//temperature_lcd.c - flashes a heartbeat LED 1s with a 50% duty cycle
+//              displays potentiometer value in 16b hex when SW1_PRESSED
+//              SW2_PRESSED samples potentiometer voltage every 1s until SW2 or SW1 pressed
+//              LCD screen displays a graphical representation of the potentio and temp value

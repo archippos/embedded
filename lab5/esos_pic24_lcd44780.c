c35182f70b0dc34e7800ac524c7c389bd5a1a205
KaneS98 <kms1165@msstate.edu>
2021-02-26 09:52:26 -0600

    Added functions already implemented by example files and to be written by us.

--- lab5/esos_pic24_lcd44780.c
+++ lab5/esos_pic24_lcd44780.c
@@ -33,37 +33,173 @@
  */
 
 /*** I N C L U D E S *************************************************/
-#include "esos_hwxxx_lcd.h"
+#include "esos_pic24_lcd44780.h"
 
 /*** T H E   C O D E *************************************************/
-void __esos_lcd44780_hwxxx_config ( void )
+void __esos_lcd44780_pic24_config ( void )
 {
-    // Set up the hardware aspects of the HWxxx interface of the LCD module service
+    // Set up the hardware aspects of the interface of the LCD module service
     //    direction, thresholds, etc beyond what is already done in esos_lcd44780_config()
 
 
 }
 
-void __esos_lcd44780_hwxxx_setDataPins( uint8_t u8_data) {
+void __esos_lcd44780_pic24_setDataPins( uint8_t u8_data) {
 	// write the hardware-specific code to take the u8_data passed in
 	// and place it on the appropriate data pins
 
 }
 
-uint8_t __esos_lcd44780_hwxxx_getDataPins( void ) {
+uint8_t __esos_lcd44780_pic24_getDataPins( void ) {
 	// write the hardware-specific code to read the appropriate data pins
 	// and create the uint8 data to return to the caller
 
 }
 
-void __esos_lcd44780_hwxxx_configDataPinsAsInput( void ) {
+void __esos_lcd44780_pic24_configDataPinsAsInput( void ) {
 	// write the hardware-specific code to set the LCD character module
 	// data pins to be "inputs" to prepare for a read of the LCD module
 
 }
 
-void __esos_lcd44780_hwxxx_configDataPinsAsOutput( void ) {
+void __esos_lcd44780_pic24_configDataPinsAsOutput( void ) {
 	// write the hardware-specific code to set the LCD character module
 	// data pins to be "outputs" to prepare for a write to the LCD module
 
+}
+
+void __esos_lcd44780_configDisplay() {
+  // initialize the LCD memory buffers
+}
+
+void __esos_lcd44780_init() {
+  // execute LCD initialization (refer to datasheet to begin power-up / refreshing screen)
+}
+
+// house-keeping functions found in esos_lcd44780.c and to be written by us
+void esos_lcd44780_clearScreen( void )
+{
+	uint8_t u8_height;
+	uint8_t u8_width;
+	// Clears the buffer
+	for(u8_height = 0; u8_height < ESOS_LCD44780_MEM_HEIGHT; u8_height++){
+		for(u8_width = 0; u8_width < ESOS_LCD44780_MEM_WIDTH; u8_width++){
+			esos_lcd44780_vars.aac_lcdBuffer[u8_height][u8_width] = ' ';
+			esos_lcd44780_vars.ab_lcdBufferNeedsUpdate[u8_height][u8_width] = TRUE;
+		}
+	}
+
+	esos_lcd44780_setCursor(0,0);
+	esos_lcd44780_vars.b_cursorPositionNeedsUpdate = TRUE;
+}
+
+void esos_lcd44780_setCursor( uint8_t u8_row, uint8_t u8_column )
+{
+    // Move cursor to (u8_row,u8_column) without changing memory buffer or the display
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_writeChar( uint8_t u8_row, uint8_t u8_column, uint8_t u8_data )
+{
+	esos_lcd44780_vars.aac_lcdBuffer[u8_row][u8_column] = u8_data;
+	esos_lcd44780_vars.ab_lcdBufferNeedsUpdate[u8_row][u8_column] = TRUE;
+}
+
+uint8_t esos_lcd44780_getChar( uint8_t u8_row, uint8_t u8_column )
+{
+	return esos_lcd44780_vars.aac_lcdBuffer[u8_row][u8_column];
+}
+
+void esos_lcd44780_writeBuffer( uint8_t u8_row, uint8_t u8_column, uint8_t *pu8_data, uint8_t u8_bufflen )
+{
+    // Write u8_bufflen characters from pu8_data to (u8_row,u8_column)
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_getBuffer( uint8_t u8_row, uint8_t u8_column, uint8_t *pu8_data, uint8_t u8_bufflen )
+{
+    // Return pu8_data with u8_bufflen characters currently displayed beginning at (u8_row,u8_column)
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_writeString( uint8_t u8_row, uint8_t u8_column, char *psz_data )
+{
+    // Write zero-terminated string psz_data to location starting at (u8_row,u8_column)
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_setCursorDisplay( BOOL u8_state )
+{
+    // Set cursor display state to u8_state
+	// TODO:  Write hardware-independent code here
+}
+
+BOOL esos_lcd44780_getCursorDisplay( void )
+{
+    // Return cursor display state
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_setCursorBlink( BOOL u8_state )
+{
+    // Set cursor blink state to u8_state
+	// TODO:  Write hardware-independent code here
+}
+
+BOOL esos_lcd44780_getCursorBlink( void )
+{
+    // Return cursor blink state
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_setDisplayVisible( BOOL u8_state )
+{
+    // Set display visible state to u8_state
+	// TODO:  Write hardware-independent code here
+}
+
+BOOL esos_lcd44780_getDisplayVisible( void )
+{
+    // Return display visible state
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_setCustomChar( uint8_t u8_charSlot, uint8_t *pu8_charData )
+{
+    // Set custom character memory for u8_charSlot to data in pu8_charData
+	// TODO:  Write hardware-independent code here
+}
+
+void esos_lcd44780_getCustomChar( uint8_t u8_charSlot, uint8_t *pu8_charData )
+{
+    // Return pu8_charData with custom character memory for u8_charSlot
+	// TODO:  Write hardware-independent code here
+}
+
+// nneded for refresh esos task
+BOOL esos_lcd44780_isCurrent( void )
+{
+	uint8_t u8_row, u8_column;
+
+	if(esos_lcd44780_vars.b_cursorPositionNeedsUpdate ||
+	   esos_lcd44780_vars.b_cursorBlinkNeedsUpdate ||
+	   esos_lcd44780_vars.b_displayVisibleNeedsUpdate) {
+		return FALSE;
+	}
+
+	for(u8_row = 0; u8_row < ESOS_LCD44780_MEM_HEIGHT; ++u8_row) {
+		for(u8_column = 0; u8_column < ESOS_LCD44780_MEM_WIDTH; ++u8_column) {
+			if(esos_lcd44780_vars.ab_lcdBufferNeedsUpdate[u8_row][u8_column]) {
+				return FALSE;
+			}
+		}
+	}
+
+	for(u8_column = 0; u8_column < ESOS_LCD44780_NUM_CUSTOM_CHARS; ++u8_column) {
+		if(esos_lcd44780_vars.ab_customCharNeedsUpdate[u8_column]) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
 }

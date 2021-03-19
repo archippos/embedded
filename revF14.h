9b06bfb7e1a709222e6eefa57f381279e756517e
archippos <deceb.z@gmail.com>
2021-03-19 17:00:06 -0500

    finished adding eeprom junk

--- revF14.h
+++ revF14.h
@@ -224,4 +224,41 @@
 	CONFIG_LCD0_AS_INPUT();		\
 }
 
+//stuff about the eeprom
+#define DS1631ADDR 0x90
+
+#define _RPOUT_SCK1OUT 6 //this is required to remap SCK1OUT
+#define CONFIG_SCK1OUT_TO_RP(Rxy_RP)  _CONFIG_SCK1OUT_TO_RP(Rxy_RP)
+#define _CONFIG_SCK1OUT_TO_RP(Rxy_RP) (_RP##Rxy_RP##R = _RPOUT_SCK1OUT)
+
+#define MCP4922_SCK (_LATD5)
+#define MCP4922_SCK_CONFIG()                                                                                           \
+    {                                                                                                                  \
+        CONFIG_RD5_AS_DIG_OUTPUT();                                                                                    \
+    }
+#define MCP4922_SDI (_LATD9)
+#define MCP4922_SDI_CONFIG()                                                                                           \
+    {                                                                                                                  \
+        CONFIG_RD9_AS_DIG_INPUT();                                                                                     \
+    }
+#define MCP4922_SDO (_LATD4)
+#define MCP4922_SDO_CONFIG()                                                                                           \
+    {                                                                                                                  \
+        CONFIG_RD4_AS_DIG_OUTPUT();                                                                                    \
+    }
+#define MCP4922_CS (_LATD0)
+#define MCP4922_CS_CONFIG()                                                                                            \
+    {                                                                                                                  \
+        CONFIG_RD0_AS_DIG_OUTPUT();                                                                                    \
+    }
+#define MCP4922_SHDN (_LATD2)
+#define MCP4922_SHDN_CONFIG()                                                                                          \
+    {                                                                                                                  \
+        /*CONFIG_RD2_AS_DIG_OUTPUT();*/                                                                                  \
+    }
+#define SLAVE_DISABLE() (_LATD0 = 1)
+#define SLAVE_ENABLE() (_LATD0 = 0)
+#define MCP4922_ON() (_LATD2 = 1)
+#define MCP4922_OFF() (_LATD2 = 0)
+
 #endif
